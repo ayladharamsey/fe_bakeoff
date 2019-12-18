2011bff1318fb1f8ad179e28bd8414e06165837f
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-12-18 14:35:27 -0700

    add commented out code so that james can read along later

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
 import { mockData } from './mockData.js'
 
+// all global variables for various dom elements that the user would interact with 
 const reportList = document.querySelector('.report-list-body')
 const sideTable = document.querySelector('.side-nav')
 const formButton = document.querySelector('#form-button')
 const resetButton = document.querySelector('#reset-button')
 
-
-window.addEventListener('load', onLoadFunctions)
+//corresponding global event listeners that fire once the user has interacted with the DOM 
+window.addEventListener('load', createSideNav)
 sideTable.addEventListener('click', viewReport)
 formButton.addEventListener('click', gatherReport)
 resetButton.addEventListener('click', resetPage)
 
 
-function onLoadFunctions() {
-    createSideNav();
-}
-
+// Upon window load, this function runs through the mock data and appends a table row for each piece of data
 function createSideNav() {
     mockData.forEach(data => {
         reportList.insertAdjacentHTML('afterbegin', 
@@ -28,6 +26,9 @@ function createSideNav() {
     }) 
 }
 
+//Upon click of a table row within the side nav, this function will go through the mock data and find the rest of the data corresponding with the option chose 
+// for this, we have to utilize event bubbling, to determine the correct data-id for the selected report
+// it then immediately invokes another function 
 function viewReport(e){
     const selectedReportId = e.target.closest('.report-selected').getAttribute('data-id')
     const correspondingData = mockData.find(data => {
@@ -36,6 +37,9 @@ function viewReport(e){
     generateDataTable(correspondingData)
 }
 
+//This function is immediately invoked in the function above
+//here, we are accessing an HTML element and injecting more HTML with the data that is passed from the previous function
+//To inject dynamic js into the appending HTML, we must use template literals 
 function generateDataTable(data){
     const table = document.querySelector('.selected-table-body')
     table.insertAdjacentHTML('afterend', 
@@ -52,6 +56,10 @@ function generateDataTable(data){
     `) 
 }
 
+//This function gathers the values that are input by the user in the form area
+//From that collected data, we create an object literal that depicts the structure of the data once submitted to local storage
+// It also immediately sends this data to another function 
+// It also immediately invokes two other functions 
 function gatherReport(e) {
     e.preventDefault()
     const inputReport = document.querySelector('#input-report-name').value
@@ -65,18 +73,37 @@ function gatherReport(e) {
 
     saveReport(inputReport, formValues)
     resetInputs()
+    renderMessage();
 }
 
+//This function actually saves our form information to Local Storage
+//We must stringify any data before setting it to local storage
+//We are setting each form by the request name as a key, then submitting the data as a value.
 function saveReport(reportName, reportInfo) {
     localStorage.setItem(`${reportName}`, JSON.stringify(reportInfo))
 }
 
+//This function is invoked immediately upon form submission to make the inputs reset back to empty
 function resetInputs(){
-    const inputReport = document.querySelector('#input-report-name').value = ''
-    const inputFrequency = document.querySelector('#input-frequency').value = ''
-    const inputDepartment = document.querySelector('#input-department-name').value = ''
+    document.querySelector('#input-report-name').value = ''
+    document.querySelector('#input-frequency').value = ''
+    document.querySelector('#input-department-name').value = ''
 }
 
+//This function is tied to the reset button on the page, so it just refreshes the page for the user. 
 function resetPage() {
     location.reload();
+}
+
+//For a better UX, I added a little alert to let the user know that their information did submit correctly. 
+// It goes away after 3 seconds
+function renderMessage() {
+    const messageLocation = document.querySelector('.success')
+    messageLocation.innerHTML = `
+        <h4> Your request was successful! <h4>
+        `
+    setTimeout(function() {
+        messageLocation.innerHTML = ''
+    }, 3000)
+    
 }
